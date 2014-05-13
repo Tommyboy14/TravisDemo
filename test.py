b26b0da374625517f9980a12412a7291ba9f2af7
Tommyboy14 <tomlinssen1@hotmail.com>
2014-05-13 10:17:16 +0200

    je eerste commit

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # Tests.py
-
+# lokaal test 1 gebruik push
 
 def test_een():
 	# Test of een plus een twee is
@@ -11,7 +11,7 @@ def test_twee():
 	
 def test_drie():
 	# Test of je door 0 kan delen
-	print 3/0
+	print 3/1
 	
 def test_vier():
 	# Meerdere checks bij elkaar
@@ -26,4 +26,4 @@ from nose.tools import *
 @raises(ZeroDivisionError) #
 def test_vijf():
 	# Deze test hoort een ZeroDivisionError te gooien
-	0/0
+	1/1
